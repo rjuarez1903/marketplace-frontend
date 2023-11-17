36e221c1fbed77ee3fb81a7a4d015b6b11b635f8
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-11-17 11:55:15 -0300

    use InputField component

--- src/components/RequestPasswordResetForm.jsx
+++ src/components/RequestPasswordResetForm.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { useFormik } from "formik";
 import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
-import CustomSnackbar from "./CustomSnackbar";
 import { requestPasswordReset } from "../api/apiService";
+import { SnackbarContext } from "../SnackbarContext";
+import InputField from "./InputField";
 
 const RequestPasswordReset = () => {
-  const [openSnackbar, setOpenSnackbar] = useState(false);
-  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const { openSnackbar, closeSnackbar } = useContext(SnackbarContext);
   const navigate = useNavigate();
 
   const formik = useFormik({
@@ -23,16 +23,13 @@ const RequestPasswordReset = () => {
       setSubmitting(true);
       try {
         await requestPasswordReset(values.email);
-        setSnackbarMessage(
-          "Si tu cuenta existe, te hemos enviado un enlace para restablecer tu contraseña."
+        openSnackbar(
+          "Si tu cuenta existe, te hemos enviado un enlace para restablecer tu contraseña.",
+          "success"
         );
-        setOpenSnackbar(true);
-        // navigate("/mis-clases");
+        navigate("/");
       } catch (error) {
-        setSnackbarMessage(
-          "Ha ocurrido un error. Por favor intenta más tarde."
-        );
-        setOpenSnackbar(true);
+        openSnackbar("Ocurrió un error. Por favor intenta más tarde.", "error");
       } finally {
         setSubmitting(false);
       }
@@ -49,32 +46,14 @@ const RequestPasswordReset = () => {
           onSubmit={formik.handleSubmit}
           className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism mx-auto"
         >
-          <div>
-            <label htmlFor="email" className="font-inter text-sm text-gray-600">
-              Correo Electrónico:
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formik.values.email}
-              onChange={(e) => {
-                formik.handleChange(e);
-                formik.setStatus(null);
-              }}
-              onBlur={formik.handleBlur}
-              className="border border-gray-300 rounded p-2 w-full"
-              // required 
-            />
-            {formik.touched.email && formik.errors.email ? (
-              <div className="text-red-500 text-xs">{formik.errors.email}</div>
-            ) : null}
-          </div>
-          {formik.status && (
-            <div className="text-red-500 text-xs p-3 bg-red-100 rounded-md">
-              {formik.status}
-            </div>
-          )}
+          <InputField
+            label="Correo Electrónico"
+            id="email"
+            name="email"
+            type="email"
+            formik={formik}
+          />
+
           <div className="flex-end mx-3 mb-5 gap-4">
             <button
               type="submit"
@@ -85,12 +64,6 @@ const RequestPasswordReset = () => {
             </button>
           </div>
         </form>
-        <CustomSnackbar
-          message={snackbarMessage}
-          type="success"
-          open={openSnackbar}
-          onClose={() => setOpenSnackbar(false)}
-        />
       </section>
     </>
   );
